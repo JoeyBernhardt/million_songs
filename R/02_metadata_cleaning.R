3b7604feeafc903eeec235a573d0502b494a607f
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2017-08-31 17:29:22 -0700

    pulling out a small number of rows to work with

--- R/02_metadata_cleaning.R
+++ R/02_metadata_cleaning.R
@@ -4,6 +4,7 @@
 library(tidyverse)
 library(dbplyr)
 library(RSQLite)
+
 # read in the metadata ----------------------------------------------------
 
 
@@ -15,6 +16,9 @@ metadata
 songs <- tbl(metadata, "songs")
 
 
+# pull out some subsets to start ------------------------------------------
+
+
 year_2010 <- songs %>%
   select(track_id, title, song_id, release, artist_id, artist_name, year, duration, artist_hotttnesss, artist_familiarity) %>% 
   filter(year > 2010) %>% 
@@ -22,3 +26,11 @@ year_2010 <- songs %>%
 
 
 write_csv(year_2010, "data-processed/metadata_snippet_2010.csv")
+
+
+subset1000 <- songs %>%
+  select(track_id, title, song_id, release, artist_id, artist_name, year, duration, artist_hotttnesss, artist_familiarity) %>%
+  head(n = 1000) %>% 
+  collect()
+
+write_csv(subset1000, "data-processed/subset1000.csv")
